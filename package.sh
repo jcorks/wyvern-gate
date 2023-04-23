9cae47354e471a54a69b5fda941db3612373d343
jcorkery <jcorkery@umich.edu>
2023-04-22 20:06:27 -0400

    Started new version of web export that interfaces with JS vm instead of emscripten-compiled C VM

--- package.sh
+++ package.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 for f in `ls -1`; do 
 	if [[ $f == *"mt" ]]; then 
-		./matte compile "$f" "./out/$f"
+		../matte/cli/matte compile "$f" "./export-web/$f"
 	fi
 done
+../matte/cli/matte compile ../matte/src/rom/core/class.mt "./export-web/Matte.Core.Class"
+../matte/cli/matte compile ../matte/src/rom/core/core.mt "./export-web/Matte.Core"
+../matte/cli/matte compile ../matte/src/rom/core/eventsystem.mt "./export-web/Matte.Core.EventSystem"
+../matte/cli/matte compile ../matte/src/rom/core/introspect.mt "./export-web/Matte.Core.Introspect"
+../matte/cli/matte compile ../matte/src/rom/core/json.mt "./export-web/Matte.Core.JSON"
+
 cd ./export-web
 GITSHORT=`git rev-parse --short HEAD`
 make GIT_SHORT_HASH=\"$GITSHORT\"
