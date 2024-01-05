3dd6baf015c52f3218c714a949ca49b188e4d408
jcorks <jcorkery@umich.edu>
2024-01-05 16:10:31 -0500

    Matched current main export

---
+++ driver.js
@@ -0,0 +1,314 @@
+(function() {
+
+
+    /*
+    @:CURSOR_ACTIONS = {
+        LEFT : 0,
+        UP : 1,
+        RIGHT : 2,
+        DOWN : 3,
+        CONFIRM : 4,
+        CANCEL : 5
+    };
+
+    */
+
+    // reads a binary file
+    const readBinary = function(name, onLoad) {
+        const req = new XMLHttpRequest();
+        req.open("GET", name, true);
+        req.responseType = "arraybuffer";
+
+        req.onload = (event) => {
+            const arrayBuffer = req.response; // Note: not req.responseText
+            if (arrayBuffer) {
+                onLoad(arrayBuffer);
+            };
+        }
+        req.send(null);
+    };
+
+    const matteList = [
+        "game_changelog",
+        "game_class.ability.mt",
+        "game_class.apparelmaterial.mt",
+        "game_class.battleai.mt",
+        "game_class.battle.mt",
+        "game_class.damage.mt",
+        "game_class.database.mt",
+        "game_class.deck.mt",
+        "game_class.die.mt",
+        "game_class.effect.mt",
+        "game_class.entity.mt",
+        "game_class.entityquality.mt",
+        "game_class.event.mt",
+        "game_class.interaction.mt",
+        "game_class.inventory.mt",
+        "game_class.island.mt",
+        "game_class.itemcolor.mt",
+        "game_class.itemdesign.mt",
+        "game_class.itemenchantcondition.mt",
+        "game_class.itemenchant.mt",
+        "game_class.item.mt",
+        "game_class.itemquality.mt",
+        "game_class.landmarkevent_cavebat.mt",
+        "game_class.landmarkevent_dungeonencounters.mt",
+        "game_class.landmarkevent_itemspecter.mt",
+        "game_class.landmarkevent.mt",
+        "game_class.landmarkevent_thebeast.mt",
+        "game_class.landmarkevent_themirror.mt",
+        "game_class.landmarkevent_treasuregolem.mt",
+        "game_class.landmark.mt",
+        "game_class.location.mt",
+        "game_class.logtimer.mt",
+        "game_class.mapentity.mt",
+        "game_class.map.mt",
+        "game_class.material.mt",
+        "game_class.party.mt",
+        "game_class.personality.mt",
+        "game_class.profession.mt",
+        "game_class.scene.mt",
+        "game_class.species.mt",
+        "game_class.stateflags.mt",
+        "game_class.state.mt",
+        "game_class.statset.mt",
+        "game_class.structuremap.mt",
+        "game_function.battlemenu.mt",
+        "game_function.correcta.mt",
+        "game_function.dice.mt",
+        "game_function.distance.mt",
+        "game_function.interactperson.mt",
+        "game_function.itemimprove.mt",
+        "game_function.itemmenu.mt",
+        "game_function.name.mt",
+        "game_function.newrecord.mt",
+        "game_function.partyoptions.mt",
+        "game_function.pickitem.mt",
+        "game_function.pickpartyitem.mt",
+        "game_function.trap.mt",
+        "game_singleton.canvas.mt",
+        "game_singleton.dungeonmap.mt",
+        "game_singleton.gamblist.mt",
+        "game_singleton.instance.mt",
+        "game_singleton.largemap.mt",
+        "game_singleton.loadableclass.mt",
+        "game_singleton.namegen.mt",
+        "game_singleton.random.mt",
+        "game_singleton.story.mt",
+        "game_singleton.windowevent.mt",
+        "game_singleton.world.mt",
+        "game_struct.battleaction.mt",
+        "game_struct.mt",
+
+        'main.external.mt',
+        'Matte.Core.Class',
+        'Matte.Core',
+        'Matte.Core.JSON',
+        'Matte.Core.Introspect',
+        'Matte.Core.EventSystem'
+
+    ];
+
+    var matteListIndex = 0;
+
+    const mattePreloadedBytecode = {};
+
+    const matte = Matte.newVM(
+        function(name) {
+            return mattePreloadedBytecode[name];
+        },
+        function(value) {
+            console.log(value);
+        },
+    );
+
+    matte.unhandledError = function(file, line, value) {
+        console.log(matte.store.valueObjectAccessString(value, 'summary').data);
+    };  
+    
+
+
+
+    matte.setExternalFunction('external_onStartCommit', [], function(fn, args) {
+        CANVAS.reset();
+        return matte.store.createEmpty();        
+    });
+
+    matte.setExternalFunction('external_onEndCommit', [],function(fn, args) {
+        return matte.store.createEmpty();            
+    });
+
+
+    matte.setExternalFunction('external_onCommitText', ['a'],function(fn, args) {
+        CANVAS.writeCommit(args[0].data);
+        return matte.store.createEmpty();        
+    });
+
+    matte.setExternalFunction('external_onSaveState', ['a', 'b'], function(fn, args) {
+        var storage = window['localStorage'];
+        const slot = args[0].data;
+        storage['wyvernslot'+slot] = args[1].data;
+        return matte.store.createEmpty();            
+    });
+
+    matte.setExternalFunction('external_onListSlots', ['a', 'b'], function(fn, args) {
+        var storage = window['localStorage'];
+        const names = Object.keys(storage);
+        const argsA = [];
+        
+        for(var i = 0; i < names.length; ++i) {
+            if (names[i].indexOf('wyvernslot' !=-1)) {
+                argsA.push(
+                    matte.store.createString(
+                        names[i].substring(
+                            10
+                        )
+                    )
+                )
+            }
+        }
+        return matte.store.createObjectArray(argsA);            
+    });
+
+      
+    matte.setExternalFunction('external_onLoadState', ['a'], function(fn, args) {
+        var storage = window['localStorage'];
+        return matte.store.createString(storage['wyvernslot'+args[0].data]);    
+    });      
+
+
+    matte.setExternalFunction('external_getInput', [], function(fn, args) {
+        if (LAST_INPUT == -1) return matte.store.createEmpty();
+        var out = LAST_INPUT;
+        LAST_INPUT = -1;
+        return matte.store.createNumber(out);
+    });
+
+
+
+    ////////// BFS_NATIVE ///////////
+    
+    const bfs_q = [];
+    const bfs_visited = [];
+    matte.setExternalFunction('wyvern_gate__native__bfs', [
+        "width",    
+        "height",
+        "scenery",
+        "start",
+        "goal"        
+    ], function(fn, args) {
+        const store = matte.store;
+
+        const width  = store.valueAsNumber(args[0]);
+        const height = store.valueAsNumber(args[1]);
+        const scenery = args[2];
+        const start = store.valueAsNumber(args[3]);
+        const goal = store.valueAsNumber(args[4]);
+    
+    
+        const aStarNewNode = function(x, y) {
+            const id = x + y*width;
+            const sceneryValue = store.valueObjectArrayAtUnsafe(scenery, id);
+            const sceneryValueNumber = store.valueAsNumber(sceneryValue);
+            if (!(sceneryValueNumber & 0x010000) && x >= 0 && y >= 0 && x < width && y < height)
+                return id;
+        }
+
+        const aStarGetNeighbors = function(neighbors, current) {
+            neighbors.length = 0;
+            const x = current%width;
+            const y = Math.floor(current/width);
+            
+            var i;
+            i = aStarNewNode(x+1, y+1); if (i != undefined) neighbors.push(i);
+            i = aStarNewNode(x+1, y-1); if (i != undefined) neighbors.push(i);
+            i = aStarNewNode(x-1, y+1); if (i != undefined) neighbors.push(i);
+            i = aStarNewNode(x-1, y-1); if (i != undefined) neighbors.push(i);
+
+            i = aStarNewNode(x-1, y  ); if (i != undefined) neighbors.push(i);
+            i = aStarNewNode(x+1, y  ); if (i != undefined) neighbors.push(i);
+            i = aStarNewNode(x  , y+1); if (i != undefined) neighbors.push(i);
+            i = aStarNewNode(x  , y-1); if (i != undefined) neighbors.push(i);
+            return neighbors;
+        }    
+    
+    
+        const q = bfs_q;
+        q.length = 0;
+        
+        const visited = bfs_visited;
+        visited.length = 0;
+        
+
+            
+        if (start == goal) return store.createEmpty();
+        var qIter = 0;
+        const neighbors = [];
+        visited[start] = start;
+        q.push(start);
+        
+        while(qIter < q.length) {
+            const v = q[qIter];
+            qIter +=1;
+
+
+            if (v == goal) {
+                // build path
+                var a = v;
+                var last;
+                const returnOut = [];
+                for(;;) {
+                    const aValue = store.createNumber(a);
+                    returnOut.push(aValue);
+                    
+                    if (visited[a] == start) {
+                        return store.createObjectArray(returnOut);
+                    }                    
+                    
+                    a = visited[a];
+                }
+            }
+            aStarGetNeighbors(neighbors, v)
+            for(var i = 0; i < neighbors.length; ++i) {
+                const w = neighbors[i];
+                if (visited[w]) continue;
+                
+                visited[w] = v; // parent
+                q.push(w);
+            }
+        }
+        
+        return store.createEmpty();
+    });
+
+//////////////////////////////////////////
+
+
+
+
+      
+    
+    var loadedCount = 0;
+    var task = setInterval(function() {
+        if (matteListIndex != matteList.length) {             
+            const mod = matteList[matteListIndex++]; 
+            readBinary(mod, function(data) {
+                console.log('Loading ' + mod);
+                mattePreloadedBytecode[mod] = data;
+                loadedCount++;
+            });
+        } 
+        
+        if (loadedCount == matteList.length) {
+            clearInterval(task);
+            const update = matte.import('main.external.mt');
+            
+            
+            setInterval(function() {
+                matte.callFunction(update, [], []);
+            }, 30);
+        }
+    });
+
+})();
+
--- index.js
+++ index.js
@@ -1,179 +1,229 @@
 var LAST_INPUT = -1;
+var CANVAS;
+(function() {
 
-/*
-@:CURSOR_ACTIONS = {
-    LEFT : 0,
-    UP : 1,
-    RIGHT : 2,
-    DOWN : 3,
-    CONFIRM : 4,
-    CANCEL : 5
-};
+    const isTouchScreen =
+      (  (('ontouchstart' in window) ||
+         (navigator.maxTouchPoints > 0) ||
+         (navigator.msMaxTouchPoints > 0)));
+    
 
-*/
+    CANVAS = {
+        _el: null,
+        _commitRow: 0,
 
-const CANVAS = {
-    _el: null,
-    _commitRow: 0,
+        init: function (element) {
+            if (!element) {
+                console.warn('Could not find canvas to init!');
+            } else {
+                this._el = element;
+            }
+        },
+        reset: function (clear = false) {
+            this._commitRow = 0;
+        },
+        clear: function () {
+            this._el.innerHTML = '';
+        },
+        writeRow: function (y, text) {
+            // The old way, but here
+            //this._el.innerHTML += '\n' + [...text]
+            //    .map(character => `<span>${character}</span>`)
+            //    .join('');
 
-    init: function (element) {
-        if (!element) {
-            console.warn('Could not find canvas to init!');
-        } else {
-            this._el = element;
-        }
-    },
-    reset: function (clear = false) {
-        this._commitRow = 0;
-    },
-    clear: function () {
-        this._el.innerHTML = '';
-    },
-    writeRow: function (y, text) {
-        // The old way, but here
-        //this._el.innerHTML += '\n' + [...text]
-        //    .map(character => `<span>${character}</span>`)
-        //    .join('');
+            // The better way
+            // Make sure row exists first
+            let row = this._el.children[y];
 
-        // The better way
-        // Make sure row exists first
-        let row = this._el.children[y];
-
-        if (!(row instanceof Element)) {
-            row = document.createElement('div')
-            row.id = `row-${y}`
-            row.className = 'canvas__row'
-        }
-
-        // Iterate over text
-        let textArray = [...text]
-        
-        textArray.forEach((character, x) => {
-            let cell = row.children[x]
-
-            if (!(cell instanceof Element)) {
-                cell = document.createElement('span')
-                cell.id = `cell-${x},${y}`
-                cell.className = 'canvas__row__cell'
+            if (!(row instanceof Element)) {
+                row = document.createElement('div')
+                row.id = `row-${y}`
+                row.className = 'canvas__row'
             }
 
-            cell.innerHTML = character;
+            // Iterate over text
+            let textArray = [...text]
+            
+            textArray.forEach((character, x) => {
+                let cell = row.children[x]
 
-            if (cell.parentElement === null) {
-                row.appendChild(cell)
+                if (!(cell instanceof Element)) {
+                    cell = document.createElement('span')
+                    cell.id = `cell-${x},${y}`
+                    cell.className = 'canvas__row__cell'
+                }
+
+                cell.innerText = character;
+
+                if (cell.parentElement === null) {
+                    row.appendChild(cell)
+                }
+            })
+
+            // Append at the end so we only render the DOM once
+            if (row.parentElement === null) {
+                this._el.appendChild(row)
             }
-        })
-
-        // Append at the end so we only render the DOM once
-        if (row.parentElement === null) {
-            this._el.appendChild(row)
+        },
+        writeCommit: function (text) {
+            this.writeRow(this._commitRow, text);
+            this._commitRow++;
         }
-    },
-    writeCommit: function (text) {
-        this.writeRow(this._commitRow, text);
-        this._commitRow++;
-    }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    CANVAS.init(document.getElementById('canvas'));
-
-    document.getElementById('arrow-left').addEventListener('click', function(e) {
-        LAST_INPUT = 0;
-    });
-
-    document.getElementById('arrow-up').addEventListener('click', function(e) {
-        LAST_INPUT = 1;
-    });
-
-    document.getElementById('arrow-right').addEventListener('click', function(e) {
-        LAST_INPUT = 2;
-    });
-
-    document.getElementById('arrow-down').addEventListener('click', function(e) {
-        LAST_INPUT = 3;
-    });
-
-
-    document.getElementById('a-button').addEventListener('click', function(e) {
-        LAST_INPUT = 4;
-    });
-
-    document.getElementById('b-button').addEventListener('click', function(e) {
-        LAST_INPUT = 5;
-    });
-
-});
-
-
-var currentGamepad = null;
-var gamepadInterval = null;
-window.addEventListener("gamepaddisconnected", function(e) {
-    if (currentGamepad === e.gamepad)
-        currentGamepad = null;
-
-    console.log("Gamepad disconnected from index %d: %s",
-        e.gamepad.index, e.gamepad.id);
-});
-
-window.addEventListener("gamepadconnected", function(e) {
-    currentGamepad = e.gamepad;
-    console.log(currentGamepad);
-    
-    if (gamepadInterval == null) {
-        gamepadInterval = setInterval(getGamepadInput, 30);
-    }
-    
-    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-        e.gamepad.index, e.gamepad.id,
-        e.gamepad.buttons.length, e.gamepad.axes.length);
-});
-
-
-var gamepadLastState = [
-    false, false, false,
-    false, false, false    
-];
-
-var gamepadThisState = [
-    false, false, false,
-    false, false, false    
-];
-
-// this isnt fit for a "realtime" game as it would 
-// eat inputs and is not flexible with axes
-var getGamepadInput = function() {
-    if (currentGamepad == null) return;
-    
-    var temp = gamepadLastState;
-    gamepadLastState = gamepadThisState;
-    gamepadThisState = temp;
-    for(var i = 0; i < 6; ++i) {
-        gamepadThisState[i] = false;
     }
 
-    if (currentGamepad.buttons[0].pressed > 0)
-        gamepadThisState[4] = true;
-    else if (currentGamepad.buttons[1].pressed > 0)
-        gamepadThisState[5] = true;
+    document.addEventListener('DOMContentLoaded', function () {
+        CANVAS.init(document.getElementById('canvas'));
+
+        if (!isTouchScreen) {
+            document.getElementById('arrow-left').addEventListener('mousedown', function(e) {
+                LAST_INPUT = 0;
+            });
+
+            document.getElementById('arrow-up').addEventListener('mousedown', function(e) {
+                LAST_INPUT = 1;
+            });
+
+            document.getElementById('arrow-right').addEventListener('mousedown', function(e) {
+                LAST_INPUT = 2;
+            });
+
+            document.getElementById('arrow-down').addEventListener('mousedown', function(e) {
+                LAST_INPUT = 3;
+            });
+
+
+            document.getElementById('a-button').addEventListener('mousedown', function(e) {
+                LAST_INPUT = 4;
+            });
+
+            document.getElementById('b-button').addEventListener('mousedown', function(e) {
+                LAST_INPUT = 5;
+            });
+        }
         
-    if (currentGamepad.axes[0].toFixed(4) > 0.6)
-        gamepadThisState[2] = true;
-    else if (currentGamepad.axes[0].toFixed(4) < -0.6)
-        gamepadThisState[0] = true;
-    else if (currentGamepad.axes[1].toFixed(4) > 0.6) {
-        gamepadThisState[3] = true;
-    } else if (currentGamepad.axes[1].toFixed(4) < -0.6) {
-        gamepadThisState[1] = true;
+        
+        
+       document.getElementById('arrow-left').addEventListener('touchstart', function(e) {
+            LAST_INPUT = 0;
+        });
+
+        document.getElementById('arrow-up').addEventListener('touchstart', function(e) {
+            LAST_INPUT = 1;
+        });
+
+        document.getElementById('arrow-right').addEventListener('touchstart', function(e) {
+            LAST_INPUT = 2;
+        });
+
+        document.getElementById('arrow-down').addEventListener('touchstart', function(e) {
+            LAST_INPUT = 3;
+        });
+
+
+        document.getElementById('a-button').addEventListener('touchstart', function(e) {
+            LAST_INPUT = 4;
+        });
+
+        document.getElementById('b-button').addEventListener('touchstart', function(e) {
+            LAST_INPUT = 5;
+        });        
+
+    });
+
+
+    var currentGamepad = null;
+    var gamepadInterval = null;
+    window.addEventListener("gamepaddisconnected", function(e) {
+        if (currentGamepad === e.gamepad)
+            currentGamepad = null;
+
+        console.log("Gamepad disconnected from index %d: %s",
+            e.gamepad.index, e.gamepad.id);
+    });
+
+    window.addEventListener("gamepadconnected", function(e) {
+        currentGamepad = e.gamepad;
+        console.log(currentGamepad);
+        
+        if (gamepadInterval == null) {
+            gamepadInterval = setInterval(getGamepadInput, 30);
+        }
+        
+        console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+            e.gamepad.index, e.gamepad.id,
+            e.gamepad.buttons.length, e.gamepad.axes.length);
+    });
+
+
+    var gamepadLastState = [
+        false, false, false,
+        false, false, false    
+    ];
+
+    var gamepadThisState = [
+        false, false, false,
+        false, false, false    
+    ];
+
+    // this isnt fit for a "realtime" game as it would 
+    // eat inputs and is not flexible with axes
+    var getGamepadInput = function() {
+        if (currentGamepad == null) return;
+        
+        var temp = gamepadLastState;
+        gamepadLastState = gamepadThisState;
+        gamepadThisState = temp;
+        for(var i = 0; i < 6; ++i) {
+            gamepadThisState[i] = false;
+        }
+
+        if (currentGamepad.buttons[0].pressed > 0)
+            gamepadThisState[4] = true;
+        else if (currentGamepad.buttons[1].pressed > 0)
+            gamepadThisState[5] = true;
+            
+        if (currentGamepad.axes[0].toFixed(4) > 0.6)
+            gamepadThisState[2] = true;
+        else if (currentGamepad.axes[0].toFixed(4) < -0.6)
+            gamepadThisState[0] = true;
+        else if (currentGamepad.axes[1].toFixed(4) > 0.6) {
+            gamepadThisState[3] = true;
+        } else if (currentGamepad.axes[1].toFixed(4) < -0.6) {
+            gamepadThisState[1] = true;
+        }
+
+        for(var i = 0; i < 6; ++i) {
+            if (gamepadThisState[i] == true && gamepadLastState[i] == false) LAST_INPUT = i;    
+        }
+        
+        if (LAST_INPUT != -1) {
+            console.log(LAST_INPUT);
+            LAST_INPUT = -1;
+        }        
     }
 
-    for(var i = 0; i < 6; ++i) {
-        if (gamepadThisState[i] == true && gamepadLastState[i] == false) LAST_INPUT = i;    
-    }
+
+
     
-    if (LAST_INPUT != -1)
-        console.log(LAST_INPUT);
-}
+
+    /*
+    const canvasInput = data.data;
+    if (typeof canvasInput == 'string') {
+        var a = document.getElementById("canvas");
+        a.innerText = 
+            'Whoops. JC made a mistake and the game pooped. Again.\n' +
+            'There\'s a BIG chance it\'s just because of a missing\n' + 
+            'feature, but just in case, you can send \'em this thingy:\n\n' +
+            canvasInput;           
+    } else {
+        for(var i = 0; i < canvasInput.length; ++i) {
+            CANVAS.writeRow(i, canvasInput[i]);
+        }    
+    }
+    */
+
+})();
+
 
 
 
@@ -195,12 +245,13 @@ var onPageInput = function (event) {
         LAST_INPUT = 3;
         break;
 
-      case 'Z': 
+      case 'z': 
       case 'Space': 
       case 'Enter': 
         LAST_INPUT = 4;
         break;
 
+      case 'x': 
       case 'Escape': 
       case 'Backspace': 
         LAST_INPUT = 5;
@@ -208,49 +259,8 @@ var onPageInput = function (event) {
 
 
     }
-    console.log(event.key);
-}
-
-
-
-
-
-WYVERN_startCommit = function() {
-    CANVAS.reset();
+    if (LAST_INPUT != -1) {
+        console.log(LAST_INPUT);
+    }
 };
-  
-WYVERN_onCommitText = function (text) {
-    CANVAS.writeCommit(text);
-};
-  
-WYVERN_onEndCommit = function() {
-        
-};
-  
-WYVERN_onSaveState = function(slot, str) {
-    var storage = window['localStorage'];
-    storage['wyvernslot'+slot] = str;
-};
-  
-WYVERN_onLoadState = function(slot) {
-    var storage = window['localStorage'];
-    return storage['wyvernslot'+slot];    
-};
-
-WYVERN_getInput = function() {
-    var out = LAST_INPUT;
-    LAST_INPUT = -1;
-    return out;
-};
-  
-WYVERN_error = function(version, error) {
-    var a = document.getElementById("canvas");
-    a.innerText = 
-        'Whoops. JC made a mistake and the game pooped. Again.\n' +
-        'There\'s a BIG chance it\'s just because of a missing\n' + 
-        'feature, but just in case, you can send \'em this thingy:\n\n' +    
-        '(commit: ' + version + ')\n' +
-        error;
-};
-
 
